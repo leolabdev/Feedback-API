559b216fc8f3dc279abd13722400f00ba971629f
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-02-19 14:37:40 +0200

    user dto updated

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,8 +1,15 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
+
 export class CreateUserDto {
     id?: number;
+    @ApiProperty({ example: 'John228', description: 'Username' })
+    @IsString()
     username: string;
+    @ApiProperty({ example: '1241413edw34', description: 'Password' })
+    @IsString()
     password: string;
     created?: Date;
     modified?: Date;
-    isValid?: boolean
+    isValid?: boolean;
 }
