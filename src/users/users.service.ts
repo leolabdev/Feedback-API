485fd58293e33077643bae8ac94746413a071aeb
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-02-19 14:48:29 +0200

    users service added

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
+
+    async handleCreateUserRequest(createUserDto: CreateUserDto): Promise<User> {
+        const user = new User();
+        user.username = createUserDto.username;
+        user.password = createUserDto.password;
+        if (createUserDto.isValid) user.isValid = createUserDto.isValid;
+        return await this.userRepository.save(user);
+
+    }
+
+
+    async handleGetUserRequest(): Promise<User[]> {
+        return await this.userRepository.find();
+
+    }
+
+    async getUserById(id: string): Promise<User> {
+        return await this.userRepository.findOneOrFail(id)
+    }
+
+
+    async deleteUser(id: string): Promise<User> {
+        const user = await this.userRepository.findOneOrFail(id,);
+        const status = await this.userRepository.delete(id);
+        return user;
+    }
+
+
+    async findUserByUserName(username: string): Promise<User> {
+        return await this.userRepository.findOne({
+            where: { username: username },
+        })
+    }
+
+
+
+
+}
