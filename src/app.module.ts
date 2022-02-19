e80df663642377f7c3ce3b456052a2761c5963f6
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-02-19 11:47:14 +0200

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController, UsersController],
   providers: [AppService],
 })
