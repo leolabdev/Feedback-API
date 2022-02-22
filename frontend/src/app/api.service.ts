da2fdcdbbf8bc90029f17180fd1f9b43ecac7784
leolab1337 <61798137+leolab1337@users.noreply.github.com>
2022-02-22 12:26:06 +0200

    added crud to frontend

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Feedback } from './feedback';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  API_SERVER = "http://localhost:3000";
+
+  constructor(private httpClient: HttpClient) { }
+
+  public readFeedbacks() {
+    return this.httpClient.get<Feedback[]>(`${this.API_SERVER}/feedbacks`);
+  }
+
+  public createFeedback(feedback: Feedback) {
+    return this.httpClient.post<Feedback>(`${this.API_SERVER}/feedbacks/create`, feedback);
+  }
+
+  public updateFeedback(feedback: Feedback) {
+    return this.httpClient.put<Feedback>(`${this.API_SERVER}/feedbacks/${feedback.id}/update`, feedback);
+  }
+
+  public deleteFeedback(id: number) {
+    return this.httpClient.delete(`${this.API_SERVER}/feedbacks/${id}/delete`);
+  }
+
+
 }
